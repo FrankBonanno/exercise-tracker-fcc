bd6720b10683ff35daeebdc1f7c7ad7aa84080b9
FrankBonanno <fb0023@mix.wvu.edu>
2024-01-04 23:35:13 -0500

    limit fixed now!

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ app.get('/api/users/:_id/logs', async (req, res) => {
     if (!logs) return res.json({ error: 'No logs found!' });
 
     if (limit) {
-      logs.splice(parseInt(limit));
+      if (!isNaN(parseInt(limit))) logs.splice(parseInt(limit));
     }
 
     if (from) {
